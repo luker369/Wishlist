ffc6320275d58307adafc23b14752276c9d3a607
luker369 <walker0369@gmail.com>
2022-12-03 18:16:49 -0600

    initial commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (<Task key={index} task={task} onDelete={onDelete} />
+      ))}
+    </>
+  )
+}
+
+export default Tasks
